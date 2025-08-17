19ea2cee1034ee1aeeed605bb5ddd306adce9be0
saniul islam <saniulislam311@gmail.com>
2025-08-17 19:13:30 +0600

    Fixed_code

--- SaniulIslam.R
+++ SaniulIslam.R
@@ -40,87 +40,127 @@ duplicated_rows <- duplicated(ld)
 
 ## 5.find normalize
 loan2 <- Loan_Datas
-# Normalize the column
-loan2$person_emp_exp_norm <- (loan2$person_emp_exp - min(loan2$person_emp_exp)) /
-  (max(ld$person_emp_exp) - min(ld$person_emp_exp))
+loan2$loan_amnt_norm <- (loan2$loan_amnt - min(loan2$loan_amnt, na.rm = TRUE)) /
+  (max(loan2$loan_amnt, na.rm = TRUE) - min(loan2$loan_amnt, na.rm = TRUE))
 
-# View first few normalized values in console
-head(loan2$person_emp_exp_norm)
-
-# View first 200 normalized values in RStudio viewer
-View(loan2$person_emp_exp_norm[1:200 ])
+print(loan2$loan_amnt_norm)
+#View(loan2$person_emp_exp_norm[1:200 ])
 
 
 
 
 ## 6.find duplicate and remove
 loan3 <- Loan_Datas
-
-# View which rows are duplicates
-duplicated_rows
-loan3[duplicated(loan3), ]
-
-distinct_data<-distinct(loan3, person_education, .keep_all = TRUE)
-
-
-# View the result
+library(dplyr)
+distinct_data <- distinct(loan3, person_education, .keep_all = TRUE)
 print(distinct_data)
-
-# View the cleaned dataset
-head(ld_unique)
-View(ld_unique[1:200, ])
 
 
 
 ## 7. View filtered data
 loan4<-Loan_Datas
-# Example: Filter rows where person_emp_exp > 10
-filtered_data <- loan4[loan4$person_emp_exp > 3.0, ]
+filtered_data <- filter(loan3, loan_amnt > 10000)
+print(filtered_data)
+filtered_data <- filter(loan3, !is.na(person_income) & person_income > 20000)
+print(filtered_data)
+filtered_data <- filter(loan3, person_education %in% c("Bachelor", "Master"))
+print(filtered_data)
 
 
-head(filtered_data)
-View(filtered_data[1:80, ])
+#head(filtered_data)
+#View(filtered_data[1:80, ])
 
 
-##5. invalid dataset and apply appropriate method
+##8. invalid dataset and apply appropriate method
+loans5<-Loan_Datas
+loans5 <- na.omit(loans5)
+cat("Number of rows after removing rows with missing values:\n")
+print(nrow(loans5))
+print(loans5)
+View(loan5[1:200, ])
+
 loan5 <- Loan_Datas  
-loa5 <-Loan_Datas
-summary(loan5$person_emp_exp)              
-
-invalid_exp <- loan5$person_emp_exp[loan5$person_emp_exp < 0 | loan5$person_emp_exp > 60]
-invalid_exp   
-View(loan5[1:199, ])
-
-# replace average value
-invalid_exp <- loa5$person_emp_exp[ loan5$person_emp_exp > 60]
-invalid_exp   
-avg_exp <- mean(loa5$person_emp_exp, na.rm = TRUE)
-print(avg_exp)
-loa5$person_emp_exp[loan5$person_emp_exp > 10] <- avg_exp
-View(loa5[1:200, ])
+loan3_clean <- na.omit(loan5)
+print(nrow(loan3_clean))
+loan3_clean
+num_cols <- sapply(loan5, is.numeric)      
+cat_cols <- sapply(loan5, is.character)    
+loan5[num_cols] <- lapply(loan5[num_cols], function(x) {
+  x[is.na(x)] <- mean(x, na.rm = TRUE)  
+  return(x)
+})
+loan5[cat_cols] <- lapply(loan5[cat_cols], function(x) {
+  mode_val <- names(sort(table(x), decreasing = TRUE))[1] 
+  x[is.na(x)] <- mode_val                                 
+  return(x)
+})
+print(colSums(is.na(loan5[num_cols])))
+print(colSums(is.na(loan5[cat_cols])))
+print(loan5)
+#View(loan5[1:200, ])
 
 
 
 ## 10. split the dataset for training and test
 #  Set a seed for reproducibility
-set.seed(123)  # ensures the same random split every time
-n <- nrow(loan5)
-
-# Randomly select 70% of rows for training
+#laons5<-Loan_Datas
+set.seed(123)
+n <- nrow(laons5)
 train_index <- sample(1:n, size = 0.7 * n)
-
-#  Create training and testing sets
-train_data <- loan5[train_index, ]   # 70% for training
-test_data  <- loan5[-train_index, ]  # remaining 30% for testing
-
-# Check dimensions
+train_data <- laons5[train_index, ]   
+test_data  <- laons5[-train_index, ] 
 dim(train_data)
 dim(test_data)
+
+###11
+loan7<-Loan_Datas
+loan_clean <- na.omit(loan5_balanced)
+descriptive_stats <- loan7 %>%
+  group_by(loan_status) %>%   
+  summarise(
+    mean_age = mean(person_age, na.rm = TRUE),
+    median_age = median(person_age, na.rm = TRUE),
+    sd_age = sd(person_age, na.rm = TRUE),
+    min_age = min(person_age, na.rm = TRUE),
+    max_age = max(person_age, na.rm = TRUE),
+    
+    mean_income = mean(person_income, na.rm = TRUE),
+    median_income = median(person_income, na.rm = TRUE),
+    sd_income = sd(person_income, na.rm = TRUE),
+    min_income = min(person_income, na.rm = TRUE),
+    max_income = max(person_income, na.rm = TRUE),
+    
+    count = n()   # number of rows per class
+  )
+
+print(descriptive_stats)
+View(loan7[1:200, ])
 
 
 
 ## 12 average value of person_emp_exp based on loan_status
 loan6<-Loan_Datas
+
+loan0 <- loan6$person_emp_exp[loan6$loan_status == 0]
+# Count of rows
+count_0 <- length(loan0)
+count_0
+avg_exp_0<-mean(loan6$person_emp_exp[loan5$loan_status == 0], na.rm=TRUE)
+avg_exp_0
+
+loan7<-Loan_Datas
+loan7
+View(loan7[1:201, ])
+
+loan1 <- loan7$person_emp_exp[loan7$loan_status == 1]
+count_1 <-length(loan1)
+count_1
+avg_exp_1<-mean(loan6$person_emp_exp[loan5$loan_status == 1], na.rm=TRUE)
+avg_exp_1
+
+## 12 average value of person_emp_exp based on loan_status
+loan6<-Loan_Datas
+
 
 loan0 <- loan6$person_emp_exp[loan6$loan_status == 0]
 # Count of rows
